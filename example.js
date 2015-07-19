52319920435ee8ed8ca3732b44fbbf0129589cca
Alberto Pose <albertopose@gmail.com>
2015-07-19 18:44:19 -0300

    Improving examples.

--- example.js
+++ example.js
@@ -1,19 +1,20 @@
 var assert = require('assert');
 var http = require('http');
 
-var iterm2ImageLoader = require('./index.js');
+var drawInIterm = require('./index.js');
 
-iterm2ImageLoader('./figure.png', function (err) {
+drawInIterm('./figure.png', function (err) {
   assert.ifError(err);
 });
 
-// URL example
+// URL + streams example
 // TODO Your image URL goes here
 // var imgUrl = '';
 // http.get(imgUrl, function(res) {
 //   if (res.statusCode === 200) {
-//     iterm2ImageLoader(res, function (err) {
+//     drawInIterm(res, function (err) {
 //       assert.ifError(err);
 //     });
 //   }
 // });
+
